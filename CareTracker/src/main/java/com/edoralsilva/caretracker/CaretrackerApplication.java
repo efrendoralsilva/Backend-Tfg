3e2eb2e392501923748b659f90523207ff6a6f0d
Efrén Doral Silva <efren.doral.silva@gmail.com>
2025-06-02 21:45:43 +0200

    Inicio del proyecto

---
+++ CareTracker/src/main/java/com/edoralsilva/caretracker/CaretrackerApplication.java
@@ -0,0 +1,13 @@
+package com.edoralsilva.caretracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaretrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaretrackerApplication.class, args);
+	}
+
+}
---
+++ CareTracker/src/test/java/com/edoralsilva/caretracker/CaretrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edoralsilva.caretracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaretrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
